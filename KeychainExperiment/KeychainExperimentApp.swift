f4dc7e3b34abcff546723b2bba3ce040b9146e99
Ferenc Nagy <ferenc.nagy@wealthsimple.com>
2023-07-27 13:20:13 -0400

    Initial Commit

---
+++ KeychainExperiment/KeychainExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeychainExperimentApp.swift
+//  KeychainExperiment
+//
+//  Created by Ferenc Nagy on 2023-07-27.
+//
+
+import SwiftUI
+
+@main
+struct KeychainExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
